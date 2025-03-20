f02bc5b87618f867ee910e0de5dc3f1061385975
anniexena <118306644+anniexena@users.noreply.github.com>
2025-03-20 17:14:44 -0400

    Init textfields with random numbers

--- Assets/ScriptsAndSuch/LumberGame/Container.cs
+++ Assets/ScriptsAndSuch/LumberGame/Container.cs
@@ -5,10 +5,16 @@ using UnityEngine.UI;
 public class Graph : MonoBehaviour
 {
     public Node[] nodes = new Node[9];
+    public GameObject[] tfObjects = new GameObject[4];
     public TextField[] textFields = new TextField[4];
 
     void Start() {
+
         // init textfields with random numbers
+        for (int i = 0; i < textFields.Length; i++) {
+            tfObjects[i] = new GameObject($"TextField_{i}");
+            textFields[i] = tfObjects[i].AddComponent<TextField>();
+        }
         foreach (TextField tf in textFields) {
             tf.randomInt = Random.Range(1,5);
         }
