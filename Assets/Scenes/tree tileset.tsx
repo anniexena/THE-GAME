ee5e750ed831a0aa44032294091b5a097721c0b9
peymae <peymae6@vt.edu>
2025-03-23 19:26:18 -0400

    tree tileset

---
+++ Assets/Scenes/tree tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tree tileset" tilewidth="32" tileheight="32" tilecount="8502" columns="109">
+ <image source="tree set.png" width="3500" height="2500"/>
+</tileset>
