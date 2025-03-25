f14146d4761847dab246adffc4627aa16231c674
GingerDeity <atreyumclewin@gmail.com>
2025-03-24 20:23:15 -0400

    Finished Rough Draft

--- Assets/Seed.cs
+++ Assets/Seed.cs
@@ -2,91 +2,146 @@ using UnityEngine;
 
 public class Seed : MonoBehaviour
 {
-    [SerializeField] Sprite[] treeSprites;
-    public int phase = 0; //default, overriden in editor for preexisting forest
-    public GameObject seed;
+    // General
+    [SerializeField] Sprite[] plantSprites; // Reference to array of sprites our plant can be
+    public Inventory playerInventory; // Reference to the player's inventory
 
     // Relating to phases of growth
-    private float growTimer = 0;
-    private float growWait = 30f;
-    private bool growTimerStart = true;
+    public int phase = 0; // Current growth phase
+    private const int MATURITY = 4; // Phase where tree can sprout seeds
+    private float growTimer = 0; // Timer for growing
+    private float growWait; // Time spent in a phase
 
-    // Relating to spawning seeds at maturity
-    private int seeds;
-    private float seedSpawnTimer = 0;
-    public float seedSpawnWait;
-    private bool seedSpawnTimerStart;
-    private bool spread = false;
-    private float wOffset = 4;
-    private float hOffset = 6.6f;
+    // Relating to spawning seeds at plant's maturity
+    private float seedSpawnTimer = 0; // Timer for growing
+    public float seedSpawnWait; // Time spent before spawning seeds
+    private float seedSpawnWoffset = 4; // x-offset for spawning seeds
+    private float seedSpawnHoffset = 6.6f; // y-offset for spawning seeds
+    private int seeds; // Number of seeds to spawn
+    private bool spread = false; // Whether we've spawned seeds yet
 
+    //Related to cutting down a plant
+    private float cutTimer = 0; // Timer for cutting
+    public float cutWait = 2.5f; // Time spent cutting
+    private bool cutTimerStart = false; // Whether we've started cutting or not
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.GetComponent<SpriteRenderer>().sprite = treeSprites[phase];
+        // Sets inventory to correct GameObject
+        playerInventory = GameObject.FindGameObjectWithTag("PlayerInventory").GetComponent<Inventory>();
+
+        // Sets sprite and collider
+        gameObject.GetComponent<SpriteRenderer>().sprite = plantSprites[phase];
+        updateCollider();
+
+        // Determines seeds and wait time for spawning them
         seeds = Random.Range(1, 5);
         seedSpawnWait = Random.Range(10, 20);
-        seedSpawnTimerStart = false;
+
+        // Determines time spent in the first phase
+        growWait = seedSpawnWait + Random.Range(20, 50);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (growTimerStart)
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y); // Updates layering
+        growTimer += Time.deltaTime;
+
+        // If our timer's gone off, increment the phase
+        if (growTimer > growWait)
         {
-            growTimer += Time.deltaTime;
-            if (growTimer > growWait && phase < treeSprites.Length)
+            // If still a phase with a sprite, update sprite, phase, collider, and reset growth timer
+            if (phase < plantSprites.Length - 1)
             {
+                gameObject.GetComponent<SpriteRenderer>().sprite = plantSprites[++phase];
+                updateCollider();
+
                 Debug.Log("Plant growing up to phase " + phase + "!");
-                gameObject.GetComponent<SpriteRenderer>().sprite = treeSprites[phase++];
+
+                // Reset grow timer vars, randomly generates time spent in next phase again
                 growTimer = 0;
+                growWait = seedSpawnWait + Random.Range(20, 50);
+            }
+            else // Otherwise, plant has died
+            {
+                Debug.Log("Plant has now died");
+                Destroy(gameObject);
             }
         }
 
-        if (phase == 4 && !spread)
-        { 
-            seedSpawnTimerStart = true;
-            //Debug.Log("we can SEED");
-        }
-
-        if (seedSpawnTimerStart)
+        // If we can spread seeds and haven't yet, set off seed-spawning timer
+        if (phase == MATURITY && !spread)
         {
             seedSpawnTimer += Time.deltaTime;
+
+            // If timer goes off, spawn seeds and update 'spread'
             if (seedSpawnTimer > seedSpawnWait)
             {
                 spawn();
                 spread = true;
-                seedSpawnTimerStart = false;
+            }
+        }
+
+        // If we've started cutting, wait until timer goes off, then destroy the plant
+        if (cutTimerStart)
+        {
+            cutTimer += Time.deltaTime;
+            if (cutTimer > cutWait)
+            {
+                // Reset timer variables
+                cutTimerStart = false;
+                cutTimer = 0;
+
+                growTimer -= cutTimer; // Prevents phase change during cutting
+                if (!spread && phase == MATURITY) { playerInventory.addSeeds(seeds); } // Plant would have seeds we could collect
+                Debug.Log("Plant cut down");
+                Destroy(gameObject);
             }
         }
     }
 
+    // If we left-click on the plant, we start cutting it down
+    private void OnMouseDown()
+    {
+        Debug.Log("Cutting Plant down");
+        cutTimerStart = true;
+    }
+
+    // Updates collider based on sprite
+    void updateCollider()
+    {
+        BoxCollider2D collider = gameObject.AddComponent<BoxCollider2D>();
+        collider.size = GetComponent<SpriteRenderer>().bounds.size; // Match sprite size
+        collider.offset = Vector2.zero; // Center the collider
+    }
+
+    // Sets the phase and updates sprite and collider
     void setPhase(int p)
     {
-        if ( p >= 0 && p <= treeSprites.Length) 
+        if (p >= 0 && p < plantSprites.Length) 
         { 
             phase = p;
-            gameObject.GetComponent<SpriteRenderer>().sprite = treeSprites[phase];
+            gameObject.GetComponent<SpriteRenderer>().sprite = plantSprites[p];
+            updateCollider();
         }
     }
 
+    // Spawns the seeds
     void spawn()
     {
-        Debug.Log("Tree disperses " + seeds);
+        Debug.Log("Plant disperses " + seeds);
         for (int i = 0; i < seeds; i++)
         {
-            float low_x = transform.position.x - wOffset;
-            float high_x = transform.position.x + wOffset;
-            float low_y = transform.position.y - hOffset;
-            float high_y = transform.position.y + hOffset;
+            float low_x = transform.position.x - seedSpawnWoffset;
+            float low_y = transform.position.y - seedSpawnHoffset;
+            float high_x = low_x + 2 * seedSpawnWoffset;
+            float high_y = low_y + 2* seedSpawnHoffset;
 
-            GameObject newSeed = Instantiate(seed, new Vector3(Random.Range(low_x, high_x),
+            GameObject newSeed = Instantiate(gameObject, new Vector3(Random.Range(low_x, high_x),
                 Random.Range(low_x, high_x), 0), transform.rotation);
-            Seed seedScript = newSeed.GetComponent<Seed>();
-
-            seedScript.setPhase(0);
-
+            newSeed.GetComponent<Seed>().setPhase(0); // Ensure it starts as a seed
         }
     }
 }
