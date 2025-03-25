72ff5aa50207dd6ffc5996c73d0527b982603843
anniexena <118306644+anniexena@users.noreply.github.com>
2025-03-24 23:10:49 -0400

    map draft is in

---
+++ Assets/Scenes/tree_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tree_tileset" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="smaller_tree.png" width="218" height="156"/>
+</tileset>
