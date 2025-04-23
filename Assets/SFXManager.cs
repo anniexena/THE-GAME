e73391792bd8b7fda70ce1cbff8d6861aa43c260
GingerDeity <131397081+GingerDeity@users.noreply.github.com>
2025-04-22 22:13:30 -0400

    added sfx manager

---
+++ Assets/SFXManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SFXManager : MonoBehaviour
+{
+    public static SFXManager instance;
+    [SerializeField] private AudioSource SFX;
+
+    private void Awake()
+    {
+        if (instance == null) { instance = this; }
+    }
+
+    public void PlaySFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        // Spawn audiosource
+        AudioSource audioSource = Instantiate(SFX, spawnTransform.position, Quaternion.identity);
+
+        // Assign necessary values + play audio
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
+
+        // Destroy after done playing
+        float length = audioSource.clip.length;
+        Destroy(audioSource.gameObject, length);
+    }
+}
