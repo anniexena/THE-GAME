2fc80856b7505e840da10b74f9930280255771d7
GingerDeity <atreyumclewin@gmail.com>
2025-04-23 11:45:10 -0400

    ADDED MUSIC :D

--- Assets/SFXManager.cs
+++ Assets/SFXManager.cs
@@ -6,11 +6,28 @@ public class SFXManager : MonoBehaviour
     public static SFXManager instance;
     public AudioSource SFX;
     public AudioClip ambianceAudio;
+    public AudioClip[] songs;
+
+    private float musicTimer = 0f;
+    private float musicWait;
 
     private void Awake()
     {
+        //musicWait = Random.Range(120, 180);
+        musicWait = Random.Range(0, 3);
         if (instance == null) { instance = this; }
         playAmbiance(ambianceAudio, transform, 1f);
+    }
+
+    void Update() {
+        musicTimer += Time.deltaTime;
+        if (musicTimer > musicWait)
+        {
+            int songChoice = Random.Range(0, songs.Length);
+            StartCoroutine(PlaySFXClipAndWait(songs[songChoice], transform, 1f));
+            musicTimer = 0;
+            musicWait = Random.Range(100, 150);
+        }
     }
 
     private void playAmbiance(AudioClip audioClip, Transform spawnTransform, float volume)
