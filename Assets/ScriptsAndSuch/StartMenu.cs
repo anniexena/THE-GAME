7b8baeff5b96aacc42a471c6885acbba1e613450
anniexena <118306644+anniexena@users.noreply.github.com>
2025-05-12 07:35:35 -0400

    better how-to-play menu

--- Assets/ScriptsAndSuch/StartMenu.cs
+++ Assets/ScriptsAndSuch/StartMenu.cs
@@ -4,10 +4,16 @@ public class StartMenu : MonoBehaviour
 {
     [SerializeField] private GameObject startMenu;
     [SerializeField] private GameObject howToMenu;
+    [SerializeField] private GameObject NatureTab;
+    [SerializeField] private GameObject TownTab;
+    [SerializeField] private GameObject BalanceTab;
+
     private void Start()
     {
         startMenu.SetActive(true);
         howToMenu.SetActive(false);
+        TownTab.SetActive(false);
+        BalanceTab.SetActive(false);
     }
     public void CloseStartMenu()
     {
@@ -16,6 +22,27 @@ public class StartMenu : MonoBehaviour
 
     public void OpenHowTo() {
         howToMenu.SetActive(true);
+    }
+
+    public void OpenNatureTab()
+    {
+        NatureTab.SetActive(true);
+        TownTab.SetActive(false);
+        BalanceTab.SetActive(false);
+    }
+
+    public void OpenTownTab()
+    {
+        NatureTab.SetActive(false);
+        TownTab.SetActive(true);
+        BalanceTab.SetActive(false);
+    }
+
+    public void OpenBalanceTab()
+    {
+        NatureTab.SetActive(false);
+        TownTab.SetActive(false);
+        BalanceTab.SetActive(true);
     }
 
     public void CloseHowTo() {
